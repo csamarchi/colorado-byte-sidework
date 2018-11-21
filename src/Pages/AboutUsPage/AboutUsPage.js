9c72da267ebd9808f6949f1023d61768019bd9fe
Christine Samarchi <csamarchi@gmail.com>
2018-11-21 14:29:23 -0600

    moved picture to top of page in About Us page, removed blue dive going through image, and moved some text below image

--- src/Pages/AboutUsPage/AboutUsPage.js
+++ src/Pages/AboutUsPage/AboutUsPage.js
@@ -6,36 +6,38 @@ class AboutUsPage extends Component {
 render() {
   return (
     <div className="about-us-container">
-      <div className="about-text-container">
-        <h1>Meet Colorado Byte</h1>
-        <p>We empower enterprises to future-proof their business and disrupt their industry.</p>
+      <div id="doing-it-right-image">
+        <img className="about-us-image" alt="man in suit" src="https://s3-us-west-2.amazonaws.com/testbucketforcb/hunters-race-408744-unsplash.jpg"/>
       </div>
       <div id="doing-it-right">
         <div id="doing-it-right-text">
           <p id="doing-title">DOING IT RIGHT</p>
           <p id="doing-text">We never compromise on our professionalism and excellence -they drive our growth.</p>
         </div>
-        <div id="doing-it-right-image">
-          <img className="about-us-image" alt="man in suit" src="https://s3-us-west-2.amazonaws.com/testbucketforcb/hunters-race-408744-unsplash.jpg"/>
-        </div>
+      </div>
+      <div className="about-text-container">
+        <h1>Meet Colorado Byte</h1>
+        <p class="empower">We empower enterprises to future-proof their business and disrupt their industry.</p>
       </div>
       <div id="entrepeneurship-container">
         <div id="entrepeneurship-text">
           <p className="section-titles" id="entrepeneurship-title">Entrepreneurship is in our DNA</p>
-          <p>We help our customers to accelerate their business. 
+          <p> ````````````````````````````````</p>
+          <span class="left">We help our customers to accelerate their business.
           <br/>
-          Our team consists of members born and bred 
-          <br/> 
+          Our team consists of members born and bred
+          <br/>
           in the startup community with capabilities to
-          <br/> 
-          scale large without scaling staff.</p>
-          <p> Although we have extensive experience in <br/> 
-          large corporate IT and IT infrastructure, <br/> 
-          the paradigm of slow and expensive technological change runs 
           <br/>
-          counter to our core philosophies.</p>
+          scale large without scaling staff.</span>
+          <span class="right"> Although we have extensive experience in <br/>
+          large corporate IT and IT infrastructure, the<br/>
+          paradigm of slow and expensive technological
+          <br/>
+          change runs counter to our core philosophies.</span>
         </div>
       </div>
+
       <div id="above-and-beyond-container">
         <div id="above-and-beyond-image-container">
           <img id="above-and-beyond-image" alt="camera parts" src="https://s3-us-west-2.amazonaws.com/testbucketforcb/vadim-sherbakov-36-unsplash.jpg"/>
@@ -46,6 +48,7 @@ render() {
           <p id="above-text">We go above and beyond for our customers on their cloud journey, providing a future-proof cloud solution every step of the way.</p>
         </div>
       </div>
+
       <div id="our-providers">
         <h2>Our Providers</h2>
         <div id="provider-image-row">
@@ -56,7 +59,7 @@ render() {
         <p>In Colorado Byte, our experience in migration and implementation of infrastructures to main public cloud platforms drive our success.</p>
         <p>We are cloud-agnostic and partners Azure, AWS, and Google Cloud.</p>
       </div>
-    </div>    
+    </div>
   );
 }
 }
